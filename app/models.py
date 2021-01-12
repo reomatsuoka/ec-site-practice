3182e27c435ed140e8381e928b5900d350720805
reomatsuoka <reo199637458@gmail.com>
2021-01-12 23:28:25 +0900

    practice

--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.IntegerField()
+    category = models.CharField(max_length=100)
+    # slugを使用すると商品ごとに割り当てたURLを使用することができる。
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.title
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+
+    def __str__(self):
+        return f"{self.item.title}：{self.quantity}"
--- app/views.py
+++ app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
+from .models import Item
+# from django.views.generic import TemplateView
+# from django.contrib.auth.mixins import LoginRequiredMixin
 
-class IndexView(LoginRequiredMixin, TemplateView):
-    template_name = 'app/index.html'
-    login_url = 'accounts/login'
+class IndexView(View):
+    # template_name = 'app/index.html'
+    # login_url = 'accounts/login'
 
+    def get(self, request, *args, **kwargs):
+        item_data = Item.objects.all()
+        return render(request, 'app/index.html',{
+            'item_data': item_data,
+        })
+
+class ItemDetailView(View):
+    def get(self, request, *args, **kwargs):
+        item_data = Item.objects.get(slug=self.kwargs['sulg'])
+
+        return render(request, 'app/product.html',{
+            'item_data': item_data,
+        })
