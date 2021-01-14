019e4c8fc36c640215b9e188e4e47aa8e4cedf05
reomatsuoka <reo199637458@gmail.com>
2021-01-14 19:39:19 +0900

    practice

--- app/templatetags/cart_tags.py
+++ app/templatetags/cart_tags.py
@@ -9,4 +9,4 @@ def itemCount(user):
         order = Order.objects.filter(user=user, ordered=False)
         if order.exists():
             return order[0].items.count()
-        return 0
+    return 0
--- app/views.py
+++ app/views.py
@@ -16,10 +16,9 @@ class IndexView(View, LoginRequiredMixin):
 
 class ItemDetailView(View):
     def get(self, request, *args, **kwargs):
-        item_data = Item.objects.get(slug=self.kwargs['sulg'])
-
-        return render(request, 'app/product.html',{
-            'item_data': item_data,
+        item_data = Item.objects.get(slug=self.kwargs['slug'])
+        return render(request, 'app/product.html', {
+            'item_data': item_data
         })
 
 @login_required
@@ -35,16 +34,36 @@ def addItem(request, slug):
     if order.exists():
         order = order[0]
         if order.items.filter(item__slug=item.slug).exists():
-            order_item.quatity += 1
+            order_item.quantity += 1
             order_item.save()
         else:
-            order_item.add(order_item)
+            order.items.add(order_item)
     else:
         order = Order.objects.create(user=request.user, ordered_date=timezone.now())
         order.items.add(order_item)
 
     return redirect('order')
 
+@login_required
+def removeItem(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order.exists():
+        order = order[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = Orderitem.objects.filter(
+                item = item,
+                user = request.user,
+                ordered=False
+            )[0]
+            order.items.remove(order_item)
+            order_item.delete()
+            return redirect("order")
+
+    return redirect("product", slug=slug)
 class OrderView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         try:
